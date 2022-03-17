1b2cbef32d36ae70514d5a5d3d0e835ab716d306
veryuniqueusername <70266105+veryuniqueusername@users.noreply.github.com>
2022-03-17 09:42:34 +0100

    preact compat

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import preact from '@preact/preset-vite'
+import { defineConfig } from 'vite';
+import preact from '@preact/preset-vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()]
-})
+	alias: {
+		react: 'preact/compat',
+		'react-dom': 'preact/compat',
+	},
+	plugins: [preact()],
+});
