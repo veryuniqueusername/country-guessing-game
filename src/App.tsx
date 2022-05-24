97ae15ee8d5a795e9b9740e329ba0017bf45867c
veryuniqueusername <70266105+veryuniqueusername@users.noreply.github.com>
2022-05-24 10:50:55 +0000

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,38 @@
-import Router, { Route } from 'preact-router';
-import NavBar from './NavBar';
-import Stopwatch from './Stopwatch';
-import Timer from './Timer';
+import { useEffect, useState } from 'preact/hooks';
+import list from './countryLocalization.json';
 
 export default function App() {
+	const [countries, setCountries] = useState<string[]>([
+		'se',
+		'ru',
+		'va',
+		'ir',
+		'de',
+	]);
+
 	return (
 		<>
-			<NavBar />
-			<Router>
-				<Route path="/timer" component={Timer} />
-				<Route path="/stopwatch" component={Stopwatch} />
-			</Router>
+			<h1>Country Guessing Game</h1>
+			<div className="Container">
+				<Country code={countries[0]} />
+				<Country code={countries[1]} />
+				<Country code={countries[2]} />
+				<Country code={countries[3]} />
+				<Country code={countries[4]} />
+			</div>
 		</>
+	);
+}
+
+type T = keyof typeof list;
+
+function Country({ code }: { code: string }) {
+	// @ts-ignore
+	const name = list[code];
+
+	return (
+		<div className="Country">
+			<p>{name}</p>
+		</div>
 	);
 }
